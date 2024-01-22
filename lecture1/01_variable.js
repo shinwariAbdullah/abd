b265aa2ab92fb6df9b9803b3c7e68a3fd7324cbf
Your Name <kahna5993@gmail.com>
2024-01-22 22:48:33 +0500

    datatypes

---
+++ lecture1/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "khan@gmail.com"
+var accountPassword = "12345"
+ accountCity = "peshawar"
+
+
+//  accountId = 2   // not allowed
+
+
+accountEmail = "shinwari@gmil.com"
+accountPassword = "78676"
+accountCity = "khyber Agnecy"
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity])    //account Table k sath hom sub log kar sakta ha
+ 
---
+++ lecture1/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strickt";    //treat all js code as never version
+
+//alert("hello")  //we are using to console window prompt
+
+
+
+let name = "Abdullah"   ///string data type
+let age = 18  //numbers
+let isloggedIn  = true 
+
+
+
+
+
+
+// numbers =>  to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+
+
+//object
+console.log(typeof null);
+console.log(typeof age);
+console.log(typeof undefined);
