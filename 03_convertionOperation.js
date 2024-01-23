7f2a2619f8fcc15e0f62b5b3e4897bd160207422
Your Name <kahna5993@gmail.com>
2024-01-23 14:03:58 +0500

    formatted

---
+++ 03_convertionOperation.js
@@ -0,0 +1,79 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1, false => 0
+
+
+
+
+// let isLoggedIn = 1
+// let boleanIslogged = Boolean(isLoggedIn)
+// console.log(boleanIslogged);
+
+
+
+
+// 1 => true; 0 => fasle
+// "" => false
+// "Abdullah" => true
+
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+
+
+//*************************************************operations**************************>>>>>>>>>>>>>>>>>>> */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+
+let str1 = "Abdullah"
+let str2 = "shinwari"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
---
+++ 04_comparison.js
@@ -0,0 +1,34 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+
+
+// console.log("2" < 1);
+// console.log("2" < 4);
+
+
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+
+
+
+  
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+
+
+
+//////===/// stricked chcecked
+console.log("2" ===2);
