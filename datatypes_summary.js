ab737c896ede1be8b7f642234d892d2e2c1deead
Your Name <kahna5993@gmail.com>
2024-01-23 14:37:32 +0500

    dataType summary

---
+++ datatypes_summary.js
@@ -0,0 +1,52 @@
+// primitive  
+// 7 types : string, num, boolean, null, undefined, symbol, bigInt,
+
+
+const score = 100
+const scorevalue = 100.3
+
+
+
+// const isloggedIn = false
+ const outsideTemp = null
+// let userEmail = undefined
+// let Email;
+
+
+
+// const Id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(Id === anotherId);
+
+
+
+
+
+
+
+
+
+// reference Type   is ko (non primitive)   B kehta ha
+
+// Array, objects, functons
+
+
+
+
+const heros = ["Abdullah","khan","shinwari"];     //{}  <= object 
+
+let myObj = {
+    name: "Abdullahkha",
+    Age: 23,
+}
+
+
+
+const myFunction = function(){
+   console.log("Abdullah khan");
+}
+
+
+
+console.log(typeof heros);
+
