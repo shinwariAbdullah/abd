25515027e182ae2233c7e28b4c00bb3ee757ea05
Your Name <kahna5993@gmail.com>
2024-01-24 13:47:07 +0500

    Dates

---
+++ 07_dates.js
@@ -0,0 +1,48 @@
+//Dates 
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+
+
+
+
+// let MycreatedDate = new Date(2024,0,24)
+// console.log(MycreatedDate.toLocaleDateString());   //new Date
+
+
+
+
+// let AbdullahDate = new Date("2024-01-24")
+// let AbdullahDate = new Date("01-24-24")
+// console.log(AbdullahDate.toLocaleString());
+
+
+
+
+
+
+
+
+//Dates Ma agr kisi ko month ya year chhiya mints hours full year etc...?
+// let NewDate = new Date()
+// console.log(NewDate);
+// console.log(NewDate.getFullYear());
+// console.log(NewDate.getHours());
+// console.log(NewDate.getTimezoneOffset());
+// console.log(NewDate.getMonth());
+// console.log(NewDate.getMinutes());
+
+
+
+
+
+NewDate.toLocaleString('default',{
+    weekday: "long",
+})
+
+
