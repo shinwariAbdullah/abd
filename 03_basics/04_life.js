62f762367fbe28d165350897715a58659a545838
Your Name <kahna5993@gmail.com>
2024-01-26 21:29:18 +0500

    immediatlu invoked functions

---
+++ 03_basics/04_life.js
@@ -0,0 +1,35 @@
+//   immediatly invoked functions Expression (Iife)
+const result = (function(a, b) {
+  //  return a + b;
+})(5, 10);
+
+//console.log(result); // Output: 15
+
+
+
+
+
+
+//another example
+(function Abdullah(){
+   // console.log(`DB connected`);      //// Video example 
+})();
+
+
+
+
+///// another example very easy to understand
+(function() {
+    //console.log('This is an IIFE');
+})();
+
+
+
+
+
+
+
+// example
+(function() {
+    console.log('This is an IIFE of Abdullah');
+})();
